c37e7ca97ff9f15f9cef59363d1a9a7f6d2c984f
Eric Li <xu.li@pillpack.com>
2021-12-13 05:37:34 -0500

    prettier

--- day12_2.rb
+++ day12_2.rb
@@ -1,46 +1,37 @@
-map={}
-seen={}
-File.readlines('day12_1.txt').each do |line| 
-    entry = line.strip.split('-')
-    if map[entry[0]].nil?
-        map[entry[0]] = []
-    end
-    if map[entry[1]].nil?
-        map[entry[1]] = []
-    end
-    map[entry[0]].append entry[1]
-    map[entry[1]].append entry[0]
-    seen[entry[0]]=0
-    seen[entry[1]]=0
+map = {}
+seen = {}
+File.readlines('day12_1.txt').each do |line|
+  entry = line.strip.split('-')
+  map[entry[0]] = [] if map[entry[0]].nil?
+  map[entry[1]] = [] if map[entry[1]].nil?
+  map[entry[0]].append entry[1]
+  map[entry[1]].append entry[0]
+  seen[entry[0]] = 0
+  seen[entry[1]] = 0
 end
 
 def find_num_path(from, to, seen, map, record)
-    if (from == to)
-        print record
-        print "\n"
-        return 1
+  if (from == to)
+    print record
+    print "\n"
+    return 1
+  end
+  result = 0
+  map[from].each do |step|
+    next if (step == 'start')
+    max_seen = seen.values.max
+    print max_seen
+    if (step == step.upcase || max_seen < 2 || seen[step] < 1)
+      seen[step] += 1 if (step == step.downcase)
+      record.push step
+      result += find_num_path(step, to, seen, map, record)
+      record.pop
+      seen[step] -= 1 if (step == step.downcase)
+    else
+
     end
-    result = 0
-    map[from].each do |step|
-        if (step == 'start') 
-            next
-        end
-        max_seen = seen.values.max
-        print max_seen
-        if (step==step.upcase || max_seen < 2 || seen[step] < 1) 
-            if (step == step.downcase) 
-                seen[step] += 1;
-            end
-            record.push step
-            result += find_num_path(step, to, seen, map, record)
-            record.pop
-            if (step == step.downcase) 
-                seen[step] -= 1;
-            end
-        else 
-        end
-    end
-    result
+  end
+  result
 end
 
-find_num_path("start", "end", seen, map, [])
+find_num_path('start', 'end', seen, map, [])
