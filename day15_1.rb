a8ddac69e5a59633f4bb27e682cd696d799e0afe
Eric Li <xulz@amazon.com>
2021-12-17 09:54:05 -0500

    example pass

--- day15_1.rb
+++ day15_1.rb
@@ -1,40 +1,65 @@
+require 'set'
+
 row = 0
 cave = []
-File.readlines('day15_1.txt').each do |line|
-#File.readlines('day15_1_example.txt').each do |line|
-   cave[row] = line.strip.split('').map{|c| Integer(c)}
-   row=row.next
+#File.readlines('day15_1.txt').each do |line|
+File.readlines('day15_1_example.txt').each do |line|
+  cave[row] = line.strip.split('').map { |c| Integer(c) }
+  row = row.next
 end
-memo = {}
-cave_map = []
-cave_map = Array.new(cave.length){Array.new(cave.length)} 
-def shortest_path(cave, start_y, start_x, memo, cave_map)
-    hash_key = start_y*1000+start_x
+cave_map = Array.new(cave.length) { Array.new(cave.length) }
 
-    if memo.has_key? hash_key
-        return memo[hash_key]
-    end
+def shortest_path(cave, start_y, start_x, cave_map, seen)
+  printf("checking Y: %s, X: %s \n", start_y, start_x)
+  unless cave_map[start_y][start_x].nil?
+    return cave_map[start_y][start_x]
+  end
 
-    len_y = cave.length
-    len_x = cave[0].length
+  len_y = cave.length
+  len_x = cave[0].length
 
-    if (start_y == len_y-1 && start_x == len_x-1)
-        return 0
-    end
+  next_cost = 1 << 16
+  potential_next_costs = []
+  up_key = (start_y - 1) * 1000 + start_x
+  down_key = (start_y + 1) * 1000 + start_x
+  left_key = start_y * 1000 + start_x - 1
+  right_key = start_y * 1000 + start_x + 1
 
-    result = 1<<16
-    if (start_x < len_x-1) 
-        result = [result, cave[start_y][start_x+1] + shortest_path(cave, start_y, start_x+1, memo, cave_map)].min
-    end
-    if (start_y < len_y-1) 
-        result = [result, cave[start_y+1][start_x] + shortest_path(cave, start_y+1, start_x, memo, cave_map)].min
-    end
-    memo[hash_key] = result
-    printf("Y: %s, X: %s , result: %s\n", start_y, start_x, result)
-    cave_map[start_y][start_x] = result + cave[start_y][start_x]
-    result
+  if start_x < len_x - 1 && !seen.include?(right_key)
+    seen.add right_key
+    potential_next_costs.push cave[start_y][start_x + 1] + shortest_path(cave, start_y, start_x + 1, cave_map, seen)
+    seen.remove right_key
+  end
+  if start_y < len_y - 1 && !seen.include?(down_key)
+    seen.add down_key
+    potential_next_costs.push cave[start_y + 1][start_x] + shortest_path(cave, start_y + 1, start_x, cave_map, seen)
+    seen.remove down_key
+  end
+  if start_x > 0 && !seen.include?(left_key)
+    seen.add left_key
+    potential_next_costs.push cave[start_y][start_x - 1] + shortest_path(cave, start_y, start_x - 1, cave_map, seen)
+    seen.remove left_key
+  end
+  if start_y > 0 && !seen.include?(up_key)
+    seen.add up_key
+    potential_next_costs.push cave[start_y - 1][start_x] + shortest_path(cave, start_y - 1, start_x, cave_map, seen)
+    seen.remove up_key
+  end
+  if start_x == len_x - 1 && start_y == len_y - 1
+    potential_next_costs.push 0
+  end
+  next_cost = potential_next_costs.min
+  cave_map[start_y][start_x] = next_cost
+  next_cost
 end
 
-print shortest_path(cave, 0, 0, memo, cave_map)
-printf ("\n")
-print cave_map
+shortest_path(cave, 0, 0, cave_map, Set.new)
+
+=begin
+(0..cave_map.length - 1).each { |i|
+  (0..cave_map[0].length - 1).each { |j|
+    printf("%s,", cave_map[i][j])
+  }
+  print "\n"
+}
+=end
--- day17_1.rb
+++ day17_1.rb
@@ -5,38 +5,42 @@ max_y = -5
 
 max_height = 0
 v_x = 1
-while v_x < max_x do
-  v_y = 1
-  while v_y < max_x do
-    simulation_max_h = 0;
+(1...151).each { |v_x|
+  (1...500).each { |v_y|
+    v_x_initial = v_x
+    v_y_initial = v_y
+    if v_x == 7 && v_y == 2
+      debug = true
+    end
+   simulation_max_h = 0
     hit_target = false
     x = 0
     y = 0
-    t = 0
-    while y > min_y do
+    t = 1
+    while y >= min_y && x <= max_x do
       x = x + v_x
       y = y + v_y
-      printf("time: %d \n", t)
-      printf("velocity: v_x %d, v_y %d\n", v_x, v_y)
-      printf("pos: x %d, y %d\n", x, y)
+
+      if debug
+        printf("time: %d \n", t)
+        printf("velocity: v_x %d, v_y %d\n", v_x, v_y)
+        printf("pos: x %d, y %d\n", x, y)
+      end
 
       simulation_max_h = [simulation_max_h, y].max
       v_x -= 1 unless v_x == 0
       v_y -= 1
       if x >= min_x && x <= max_x && y >= min_y && y <= max_y
         hit_target = true
-        printf(">>>>>>>>>>>>>>>>> Hit at time %s <<<<<\n", t)
+        printf(">>>>>>>>>>>>>>>>> Hit at time %s at x %d y %d<<<<<<<<<<<<<<\n", t, x, y)
+        printf("initial speed x: %d, y: %d\n", v_x_initial, v_y_initial)
+        break
       end
-      t = t + 1
-      printf("----------------time: %d \n", t)
-      printf("velocity: v_x %d, v_y %d\n", v_x, v_y)
-      printf("pos: x %d, y %d\n", x, y)
+      t += 1
     end
-    #printf("simulation highest: %s\n", simulation_max_h)
     max_height = [max_height, simulation_max_h].max if hit_target
-    v_y = v_y + 1
-  end
-  v_x = v_x + 1
-end
+    printf("simulation highest: %s\n", simulation_max_h) if hit_target
+  }
+}
 
 print max_height
