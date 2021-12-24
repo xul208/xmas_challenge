6a398e0b630d40e7346b1f7b5f67c40a86bad7cb
Eric Li <xu.li@pillpack.com>
2021-12-24 13:28:55 -0500

    d24 pattern analysis

--- src/Day22Solution.java
+++ src/Day22Solution.java
@@ -12,6 +12,7 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day22Solution {
     public static void main(String[] args) {
@@ -20,6 +21,7 @@ public class Day22Solution {
             reader = new BufferedReader(new FileReader("d22_1.txt"));
             String line = reader.readLine();
             List<List<Integer>> operations = new ArrayList<>();
+            int[] boundary = {0, 0, 0, 0, 0, 0};
             while (line != null) {
                 List<Integer> entry = new ArrayList<>(7);
                 Pattern pattern = Pattern.compile("^(.*) x=(-?\\d+)..(-?\\d+),y=(-?\\d+)..(-?\\d+),z=(-?\\d+)..(-?\\d+)$");
@@ -33,12 +35,18 @@ public class Day22Solution {
                     } else {
                         entry.add(0);
                     }
+                    boundary[0] = Math.min(boundary[0], entry.get(0));
+                    boundary[1] = Math.max(boundary[1], entry.get(1));
+                    boundary[2] = Math.min(boundary[2], entry.get(2));
+                    boundary[3] = Math.max(boundary[3], entry.get(3));
+                    boundary[4] = Math.min(boundary[4], entry.get(4));
+                    boundary[5] = Math.max(boundary[5], entry.get(5));
                 }
                 operations.add(entry);
                 line = reader.readLine();
             }
 
-            System.out.println(question2((operations)));
+            System.out.println(question1((operations)));
 
         } catch (Exception e) {
             System.out.println(
@@ -49,19 +57,26 @@ public class Day22Solution {
 
     public static int question1(List<List<Integer>> operations) {
         int lightCount = 0;
-        for (int x = -50; x <= 50; ++x) {
-            for (int y = -50; y <= 50; ++y) {
-                for (int z = -50; z <= 50; ++z) {
-                    int light = 0;
-                    for (var operation : operations
-                    ) {
-                        if (x >= operation.get(0) && x <= operation.get(1) && y >= operation.get(2) && y <= operation.get(3) &&
-                                z >= operation.get(4) && z <= operation.get(5)) {
-                            light = operation.get(6);
+        for (int x = -120100; x <= 120875; ++x) {
+            var date=java.util.Calendar.getInstance().getTime();
+            System.out.println(date);
+            System.out.printf("current x: %d, ===>%d \n", x, lightCount );
+            for (int y = -124565; y <= 118853; ++y) {
+                final int xx = x; final int yy = y;
+                int lights = IntStream.range(-121762, 119054).parallel().map(z -> {
+                        int light = 0;
+                        for (int i= operations.size()-1; i>=0; --i){
+                            var operation = operations.get(i);
+                            if (xx >= operation.get(0) && xx <= operation.get(1) && yy >= operation.get(2) && yy <= operation.get(3) &&
+                                    z >= operation.get(4) && z <= operation.get(5)) {
+                                light = operation.get(6);
+                                System.out.println(i);
+                                break;
+                            }
                         }
-                    }
-                    lightCount += light;
-                }
+                        return light;
+                    }).reduce(Integer::sum).getAsInt();
+                lightCount += lights;
             }
         }
         return lightCount;
@@ -69,49 +84,31 @@ public class Day22Solution {
 
     public static BigInteger question2(List<List<Integer>> operations) {
         Map<Integer, Cubic> registry = new HashMap<>();
-        int MAX_LEN = 100_000;
+        int MAX_LEN = 1_000_000;
         List<Integer> space = Arrays.asList(-MAX_LEN, MAX_LEN, -MAX_LEN, MAX_LEN, -MAX_LEN, MAX_LEN, 0);
         Cubic cubic = new Cubic(space);
         registry.put(cubic.hashKey(), cubic);
         List<Integer> xAxis = new ArrayList<>();
-        Set<Integer> xSeen = new HashSet<>();
         List<Integer> yAxis = new ArrayList<>();
-        Set<Integer> ySeen = new HashSet<>();
         List<Integer> zAxis = new ArrayList<>();
-        Set<Integer> zSeen = new HashSet<>();
 
-        xSeen.add(cubic.stub.get(0));
         xAxis.add(cubic.stub.get(0));
-        xSeen.add(cubic.stub.get(1));
         xAxis.add(cubic.stub.get(1));
 
-        ySeen.add(cubic.stub.get(2));
         yAxis.add(cubic.stub.get(2));
-        ySeen.add(cubic.stub.get(3));
         yAxis.add(cubic.stub.get(3));
 
-        zSeen.add(cubic.stub.get(4));
         zAxis.add(cubic.stub.get(4));
-        zSeen.add(cubic.stub.get(5));
         zAxis.add(cubic.stub.get(5));
 
         for (var op : operations) {
             System.out.println(op);
-            if(!xSeen.contains(op.get(0)-1)) {
-                xSeen.add(op.get(0)-1)
-                xAxis.add(op.get(0) - 1);
-            }
             xAxis.add(op.get(0));
             xAxis.add(op.get(1));
-            xAxis.add(op.get(1) + 1);
-            yAxis.add(op.get(2) - 1);
             yAxis.add(op.get(2));
             yAxis.add(op.get(3));
-            yAxis.add(op.get(3) + 1);
-            zAxis.add(op.get(4) - 1);
             zAxis.add(op.get(4));
             zAxis.add(op.get(5));
-            zAxis.add(op.get(5) + 1);
             Collections.sort(xAxis);
             Collections.sort(yAxis);
             Collections.sort(zAxis);
@@ -119,9 +116,9 @@ public class Day22Solution {
             Set<Integer> seen = new HashSet<>();
             Cubic cubicFromOp = new Cubic(op);
 
-            for (int i = 0; i < xAxis.size() - 1; i += 2) {
-                for (int j = 0; j < yAxis.size() - 1; j += 2) {
-                    for (int k = 0; k < zAxis.size() - 1; k += 2) {
+            for (int i = 0; i < xAxis.size() - 1; i += 1) {
+                for (int j = 0; j < yAxis.size() - 1; j += 1) {
+                    for (int k = 0; k < zAxis.size() - 1; k += 1) {
                         List<Integer> currentCubicStub = Arrays.asList(xAxis.get(i), xAxis.get(i + 1),
                                 yAxis.get(j), yAxis.get(j + 1), zAxis.get(k), zAxis.get(k + 1), 0);
                         Cubic currentCubic = new Cubic(currentCubicStub);
@@ -170,9 +167,9 @@ public class Day22Solution {
             if (stub.get(6) == 0) {
                 return BigInteger.ZERO;
             }
-            return BigInteger.valueOf(stub.get(1) - stub.get(0)).multiply(
-                    BigInteger.valueOf(stub.get(3) - stub.get(2))).multiply(
-                    BigInteger.valueOf(stub.get(5) - stub.get(4)));
+            return BigInteger.valueOf(stub.get(1) - stub.get(0) + 1).multiply(
+                    BigInteger.valueOf(stub.get(3) - stub.get(2) + 1)).multiply(
+                    BigInteger.valueOf(stub.get(5) - stub.get(4) + 1));
         }
 
         public int hashKey() {
---
+++ src/Day24Solution.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+import static java.util.Map.copyOf;
+
+public class Day24Solution {
+    public static void main(String[] args) {
+        BufferedReader reader;
+        List<List<String>>operations = new ArrayList<>();
+        try {
+            reader = new BufferedReader(new FileReader("day24_1.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                List<String> operation = Arrays.asList(line.split(" "));
+                operations.add(operation);
+                line = reader.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(operations);
+        System.out.println(question1(operations));
+    }
+
+    public static long question1(List<List<String>> ops) {
+        return 0;
+    }
+
+    public static Map<Character, Integer> iteration(Map<Character, Integer> prev) {
+        /*
+inp w       w = new_num
+mul x 0
+add x z
+mod x 26
+div z 1     z = z              OR   z/26
+add x 12    x = z % 26 + (>10) OR   + (<0)
+eql x w
+eql x 0     x = x != w ? 1 : 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y     z *= ((25 * x) + 1)
+mul y 0
+add y w
+add y 8
+mul y x
+add z y     z += (w+(8)) * x
+
+if z is zero, the next round z will be non-zero
+if "offset1 > 10", z will be   +=(w + offset2)
+         */
+    }
+
+    public static boolean isValidate(long ver, List<List<String>> ops) {
+        String verStr = String.valueOf(ver);
+        if(verStr.contains("0")) return false;
+        return false;
+    }
+}
